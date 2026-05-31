f6a59107da23795772640140ea32b82cb579aecd
ShervinSalaah <shervinsalaah@gmail.com>
2026-05-31 18:15:51 +0530

    recompilation

--- src/core.h
+++ src/core.h
@@ -1,11 +1,12 @@
-#ifndef CORE_H 
-#define CORE_H 
-#include "config.h" 
- 
-void initSystem(Config *cfg); 
-int displayMainMenu(); 
-void loadConfig(Config *cfg); 
-void saveConfig(const Config *cfg); 
-int validateFilePath(const char *path); 
- 
-#endif 
+#ifndef CORE_H
+#define CORE_H
+
+#include "config.h"
+
+void initSystem(Config *cfg);
+int displayMainMenu();
+void loadConfig(Config *cfg);
+void saveConfig(const Config *cfg);
+int validateFilePath(const char *path);
+
+#endif
